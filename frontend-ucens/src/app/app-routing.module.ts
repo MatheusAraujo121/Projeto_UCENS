741ba9aa9335aef35108ac568fdc6f8d032eaff6
Matheus <mathujo453@gmail.com>
2025-07-26 14:42:31 -0300

    feat: adding a login page
    description: Adding a new component page for the project, the login page
    
    name: Matheus de A. Emidio

--- frontend-ucens/src/app/app-routing.module.ts
+++ frontend-ucens/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { StoryComponent } from './pages/institutional/story/story.component';
+import { LoginComponent } from './pages/system/admin/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },   
-  { path: 'story', component: StoryComponent }
+  { path: 'story', component: StoryComponent },
+  { path: 'login', component: LoginComponent }
    
 ];
 
