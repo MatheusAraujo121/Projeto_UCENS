21c2b6f4de234992de7aa205fceca67183a2cb6e
MATHEUS DE ARAUJO EMIDIO <0031432412023@lti.fatecso.edu.br>
2025-09-09 17:53:19 -0300

    fix: fixing header
    description: fixing some bugs on header
    
    name: Matheus de A. Emidio

--- backend-ucens/obj/Debug/net8.0/userApi.AssemblyInfo.cs
+++ backend-ucens/obj/Debug/net8.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87611b71a7bd1de6c3bb621e0fef2bd2e26a6ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0a51d89d1f6477dea9f0e113c60cc3d2ad3266")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
